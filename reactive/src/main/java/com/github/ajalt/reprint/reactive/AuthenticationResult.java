109364a2c3d99b392be628c4a11be520ca01d8f5
AJ <aj@alt>
2017-04-23 21:29:29 +0100

    Add nullability annotations.

--- reactive/src/main/java/com/github/ajalt/reprint/reactive/AuthenticationResult.java
+++ reactive/src/main/java/com/github/ajalt/reprint/reactive/AuthenticationResult.java
@@ -1,5 +1,6 @@
 package com.github.ajalt.reprint.reactive;
 
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import com.github.ajalt.reprint.core.AuthenticationFailureReason;
@@ -16,6 +17,7 @@ public class AuthenticationResult {
      * events will be emitted. Other statuses are terminal events, and the fingerprint sensor will
      * be stopped by the time they are emitted.
      */
+    @NonNull
     public final Status status;
     /**
      * The general reason for the failure.
@@ -30,6 +32,7 @@ public class AuthenticationResult {
      * <p>
      * This string is never null, and will be localized to the current locale.
      */
+    @NonNull
     public final CharSequence errorMessage;
     /**
      * The {@link ReprintModule#tag()} of the module that is currently active. This is useful to
@@ -42,17 +45,12 @@ public class AuthenticationResult {
      */
     public final int errorCode;
 
-    public AuthenticationResult(Status status, @Nullable AuthenticationFailureReason failureReason,
-                                CharSequence errorMessage, int fromModule, int errorCode) {
+    public AuthenticationResult(@NonNull Status status, @Nullable AuthenticationFailureReason failureReason,
+                                @NonNull CharSequence errorMessage, int fromModule, int errorCode) {
         this.status = status;
         this.failureReason = failureReason;
         this.errorMessage = errorMessage;
         this.fromModule = fromModule;
         this.errorCode = errorCode;
-    }
-
-    /** Return true if the authentication was a success. */
-    public boolean isSuccessful() {
-        return failureReason != null;
     }
 }
